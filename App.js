173e1f24df5cd2b95129dda6a57f95d06bf0ed01
TuTuanLe <tutuanletkpt@gmail.com>
2022-12-01 23:28:32 +0700

    add editor

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { readAsStringAsync } from 'expo-file-system';
+import { WebView } from 'react-native-webview';
+import { useAssets } from 'expo-asset';
+import { useState } from 'react';
+
+const exampleHTML = require('./simple-image/example.html');
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    console.log('PolicyHTML:', exampleHTML);
+    const [index, indexLoadingError] = useAssets(require('./simple-image/example.html'));
+    // const [html, setHtml] = useState('');
+
+    // if (index) {
+    //     readAsStringAsync(index[0].localUri).then((data) => {
+    //         setHtml(data);
+    //     });
+    // }
+    return (
+        <WebView
+            style={styles.container}
+            source={require('./simple-image/example.html')}
+            originWhitelist={'["*"]'}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        marginTop: 40,
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
